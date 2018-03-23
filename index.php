cb9f752f7e874029aebb57287879af21e779083b
ernestoff <ernesto.fatuarte@enreda.coop>
2018-03-23 14:11:36 +0100

    Add firms list

---
+++ index.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Sevilla X El Cambio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="include/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Mailchimp CSS -->
+    <link href="include/css/mailchimp.css" rel="stylesheet">
+    <!-- Lato font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <!-- Social Share Kit CSS -->
+    <link rel="stylesheet" href="include/css/social-share-kit.css" type="text/css">
+    <!-- Custom styles for this template -->
+    <link href="include/css/sticky-footer-navbar.css" rel="stylesheet">
+    <meta property="og:site_name" content="Sevilla X El Cambio">
+    <meta property="og:image" content="include/images/logo-transparente.png">
+  </head>
+
+  <body>
+
+   <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header text-center">
+        <img src="include/images/logo-transparente.png"/>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="row" id="text-manifiest">
+        <div class="col-md-7 text-justify" id="manifiesto">
+          <h2>Confluir para el cambio en Sevilla</h2>
+          <p>Sevilla, la cuarta ciudad con más población de todo el Estado, necesita un cambio
+  político que aborde las graves carencias que padece. Un cambio que desde una
+  perspectiva progresista ponga a las personas en el eje de las políticas municipales.
+  Tenemos las experiencias de Ayuntamientos como Cádiz, Madrid, Zaragoza, Barcelona,
+  Coruña... que están siendo gestionados por un municipalismo de cambio y que
+  constituyen una referencia. En ese sentido, saludamos e impulsamos una confluencia
+  andaluza municipalista.</p>
+  <p>Nuestra ciudad presenta un modelo económico que no permite combatir con solvencia
+  el desempleo estructural y la precariedad laboral que sufrimos desde hace décadas.
+  Seguimos teniendo a siete de los 15 barrios más empobrecidos de todo el estado. Los
+  servicios sociales están colapsados y no cuentan con suficientes recursos económicos
+  para garantizar el rescate ciudadano. Y encima se devuelven partidas por falta de
+  recursos humanos. Nuestros barrios no disponen, en muchos casos, de infraestructuras
+  adecuadas (colegios, centros de salud, espacios deportivos, parques...) que permitan una
+  convivencia de calidad. Diariamente se producen desahucios en nuestra ciudad.</p>
+  <p>El malestar de miles de personas nos obligan a dar respuestas y confluir para construir
+  una alternativa de cambio. Es necesario desde un debate democrático y honesto repensar
+  el proyecto de ciudad para imaginar la Sevilla que queremos. Una ciudad del bien común,
+  ecológica, con derechos, igualitaria y feminista, participativa, viva... necesita de una
+  articulación cultural, social y política que concite la confianza y enamore a la ciudadanía.
+  Es momento de superar lógicas de competencia, apostar por dinámicas participativas,
+  horizontales y colaborativas para generar un proceso inclusivo y respetuoso con las
+  minorías y la pluralidad.</p>
+  <p>Por eso llamamos a las plataformas vecinales y a personas a título individual; a los
+  movimientos sociales, al sindicalismo, al feminismo, al ecologismo, al pacifismo; a las
+  fuerzas de la cultura y del arte; a los partidos y colectivos políticos de izquierda
+  transformadora a unirse y participar para poner en marcha este proceso.</p>
+  <p>Hay sobrados motivos para convocarnos las fuerzas del cambio a construir una
+  esperanza de futuro para las sevillanas y sevillanos. Hay que hacerlo.</p>
+
+  <strong>DIEZ EJES PARA PENSAR EL CAMBIO</strong>
+  <p>Un modelo de ciudad al servicio de las personas. Una economía alternativa y solidaria que
+  aborde desde el respeto a los derechos humanos las necesidades de las personas, generando
+  empleo sostenible, estable y de calidad.</p>
+  <ul>
+  <li>Una ciudad ecologista, habitable, accesible, inteligente... que proteja la biodiversidad, apueste
+  por energías limpias, promueva el reciclaje e impulse una movilidad sostenible.</li>
+  <li>Una ciudad feminista que luche activamente contra la violencia machista y el patriarcado</li>
+  <li>Una ciudad justa e inclusiva que garantice los suministros básicos y combata la pobreza</li>
+  <li>Una ciudad democrática donde los barrios y las personas sean protagonistas de sus decisiones,
+  con un Ayuntamiento descentralizado con paredes de cristal, que impulse la democracia
+  participativa.</li>
+  <li>Una ciudad de derechos que garantice unos servicios públicos eficientes, una educación de
+  calidad, una sanidad universal.</li>
+  <li>Una ciudad para la democracia cultural y la expresión artística.</li>
+  <li>Una ciudad abierta que permita la integración social de las personas migrantes.</li>
+  <li>Una ciudad libre de desahucios, que posibilite el acceso a una vivienda digna.</li>
+  <li>Una ciudad de los barrios que acabe con las desigualdades existentes en recursos y servicios.</li>
+        </div>
+        <div class="col-md-4 col-md-offset-1" id="apoya">
+          <!-- Begin MailChimp Signup Form -->
+          <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+          <style type="text/css">
+            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+          </style>
+          <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://participasevilla.us15.list-manage.com/subscribe/post?u=c9cf8c1f269ce80cc42e1289b&amp;id=3cb232708a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+<div class="indicates-required"><span class="asterisk">*</span> campo requerido</div>
+<div class="mc-field-group">
+  <label for="mce-FNAME">Nombre y apellidos<span class="asterisk">*</span>
+</label>
+  <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+  <label for="mce-EMAIL">Correo electrónico<span class="asterisk">*</span>
+</label>
+  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group">
+  <label for="mce-ENTITY">Entidad
+</label>
+  <input type="text" value="" name="ENTITY" id="mce-ENTITY">
+</div>
+<div class="mc-field-group input-group">
+    <label>¿Desea apoyar publicamente el manifiesto? </label>
+    <ul><li><input type="checkbox" value="No" name="SUPPORT" id="mce-SUPPORT-0"><label for="mce-SUPPORT-0">No, no mostrar mi apoyo publicamente</label></li>
+</ul>
+</div>
+  <div id="mce-responses" class="clear">
+    <div class="response" id="mce-error-response" style="display:none"></div>
+    <div class="response" id="mce-success-response" style="display:none"></div>
+  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c9cf8c1f269ce80cc42e1289b_3cb232708a" tabindex="-1" value=""></div>
+    <div class="text-center"><input type="submit" value="Firmar el manifiesto" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+<!--End mc_embed_signup-->
+          <!--End mc_embed_signup-->
+        <div class="ssk-xs ssk-group" data-url="http://sevillaxelcambio.info" data-text="¿Conoces sevillaXelcambio? +Info en www.sevillaxelcambio.info">
+            <span class="text-muted"> Compartir: </span>
+            <a href="" class="ssk ssk-facebook"></a>
+            <a href="" class="ssk ssk-twitter"></a>
+            <a href="" class="ssk ssk-google-plus"></a>
+            <a href="" class="ssk ssk-email"></a>
+            <a href="" class="ssk ssk-whatsapp"></a>
+            <a href="" class="ssk ssk-link"></a>
+        </div>
+        </div>
+      </div>
+
+      <?php
+
+      $api_key = '';
+      $list_id = '';
+
+      $dc = substr($api_key,strpos($api_key,'-')+1);
+
+      $url = 'https://'.$dc.'.api.mailchimp.com/3.0/lists/'.$list_id.'/members';
+      $body = json_decode(rudr_mailchimp_curl_connect($url, 'GET', $api_key, $data));
+
+      foreach ($body->members as $member) {
+      	if ($member->merge_fields->SUPPORT!='No')  {
+      		$resultado[] = array(Nombre=>$member->merge_fields->FNAME, Entidad=>$member->merge_fields->ENTITY);
+      	  echo "Nombre: " . $member->merge_fields->FNAME . " ";
+      	  echo "Entidad: " . $member->merge_fields->ENTITY . " ";
+      	  echo "<br>";
+      	}
+      }
+
+
+
+      function rudr_mailchimp_curl_connect( $url, $request_type, $api_key, $data = array() ) {
+      	if( $request_type == 'GET' )
+      		$url .= '?' . http_build_query($data);
+
+      	$mch = curl_init();
+      	$headers = array(
+      		'Content-Type: application/json',
+      		'Authorization: Basic '.base64_encode( 'user:'. $api_key )
+      	);
+      	curl_setopt($mch, CURLOPT_URL, $url );
+      	curl_setopt($mch, CURLOPT_HTTPHEADER, $headers);
+      	//curl_setopt($mch, CURLOPT_USERAGENT, 'PHP-MCAPI/2.0');
+      	curl_setopt($mch, CURLOPT_RETURNTRANSFER, true); // do not echo the result, write it into variable
+      	curl_setopt($mch, CURLOPT_CUSTOMREQUEST, $request_type); // according to MailChimp API: POST/GET/PATCH/PUT/DELETE
+      	curl_setopt($mch, CURLOPT_TIMEOUT, 10);
+      	curl_setopt($mch, CURLOPT_SSL_VERIFYPEER, false); // certificate verification for TLS/SSL connection
+
+      	if( $request_type != 'GET' ) {
+      		curl_setopt($mch, CURLOPT_POST, true);
+      		curl_setopt($mch, CURLOPT_POSTFIELDS, json_encode($data) ); // send data in json
+      	}
+
+      	return curl_exec($mch);
+      }
+
+      ?>
+
+    </div> <!-- /container -->
+
+    <footer class="footer">
+      <div class="container text-center">
+        <img src="include/images/logo-transparente.png"/>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="include/js/vendor/popper.min.js"></script>
+    <script src="include/js/bootstrap.min.js"></script>
+    <!-- Social Share Kit JS -->
+    <script type="text/javascript" src="include/js/social-share-kit.js"></script>
+  </body>
+
+  <script type="text/javascript">
+  SocialShareKit.init({
+
+  });
+  </script>
+</html>
